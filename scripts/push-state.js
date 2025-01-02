8ca54371db044490e2bd5ac398cfd97410f897fc
Shabbir Ali <86016840+codeliya@users.noreply.github.com>
2025-01-02 13:39:21 +0530

    Delete the file at 'scripts/push-state.js'

--- scripts/push-state.js
+++
@@ -1,9 +0,0 @@
-! function (){
- var t;
- try{for (t=0; 10 > t; ++t)
-  history.pushState({}, "", "#");
- onpopstate=function (t)
- {
-  t.state && alert('अपने दोस्तों के साथ शेयर करना न भूले ');
-  }}
-  catch (o){}}();
